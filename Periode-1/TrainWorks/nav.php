466e61758b79c05531197f62ce7cf5e76ff01b51
Don Kieu <2177651@talnet.nl>
2024-09-12 14:47:29 +0200

    begin nav

--- Periode-1/TrainWorks/nav.php
+++ Periode-1/TrainWorks/nav.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/nav.css">
 </head>
+
 <body>
     <div class="nav">
-        <img src="logo.png">
-        <a href="homepage.php">Homepage</a>
-        <a href="index.php">Rekenmachine</a>
+        <div class="embleem">
+            <img src="logo.png">
+        </div>
+
+        <div class="links">
+            <a href="homepage.php">Homepage</a>
+            <a href="index.php">Rekenmachine</a>
+        </div>
     </div>
 </body>
 </html>
