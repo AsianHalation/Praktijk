2b8a84209c3f80cea6d6c5928ec8fb6825300c48
Don Kieu <2177651@talnet.nl>
2024-09-15 16:15:54 +0200

    simpelere rekenmachine
    meer functies in operators

--- Periode-1/TrainWorks/functions.php
+++ Periode-1/TrainWorks/functions.php
@@ -1,12 +1,32 @@
 <?php
 
-if(isset($_POST['count'])){
-    //pakt data uit formulier
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
-    $leeftijd = $_POST['leeftijd'];
-    $dagen = $_POST['dagen'];
+    $operator = $_POST['operator'];
 
-    $result = ($num1 - $num2) * $dagen;
+    switch ($operator) {
+        case 'add':
+            $result = $num1 + $num2;
+            break;
+        case 'subtract':
+            $result = $num1 - $num2;
+            break;
+        case 'multiply':
+            $result = $num1 * $num2;
+            break;
+        case 'divide':
+            if ($num2 != 0) {
+                $result = $num1 / $num2;
+            } else {
+                $result = "Cannot divide by zero";
+            }
+            break;
+        default:
+            $result = "Invalid operator";
+            break;
+    }
+
+    echo "<h2>Result: $result</h2>";
 }
 ?>
--- Periode-1/TrainWorks/rekenmachine.php
+++ Periode-1/TrainWorks/rekenmachine.php
@@ -27,36 +27,21 @@
     </header>
 
     <main>
-        <form method="POST" action="rekenmachine.php">
-            <legend>Bereken hier je eetpatroon</legend>
-
-            <label for="leeftijd">Wat is uw leeftijd?</label>
-            <input type="number" name="leeftijd">
-
-            <label for="num1">Dagelijkse inname in Kcal</label>
-            <input type="number" class="num" name="num1">
-
-            <label for="dagen">Voor hoeveel dagen wil je bereken?</label>
-            <input type="number" name="dagen">
-
-            <label for="num2">Dagelijkse verbranding in Kcal</label>
-            <input type="text" class="num" name="num2">
-
-            <input type="submit" name=count value="bereken!">
-
+        <form method="post" action="rekenmachine.php">
+            <input type="text" name="num1" placeholder="Enter number 1" required>
+            <select name="operator">
+                <option value="add">+</option>
+                <option value="subtract">-</option>
+                <option value="multiply">*</option>
+                <option value="divide">/</option>
+            </select>
+            <input type="text" name="num2" placeholder="Enter number 2" required>
+            <br>
+            <input type="submit" name="count" value="Calculate">
         </form>
-            
-            <?php
-                if (isset($_POST['count'])) {
-                    echo   "<p>{$result} is uw totale inname over {$dagen} dagen.</p>
-                            <br>";
-                    if ($result < (2000 * $dagen)) {
-                        echo "<p>Jij bent gezond bezig</p>";
-                    } else {
-                        echo "<p>Jij bent eet meer dan het dan het gemiddelde</p>";
-                    }
-                }
-            ?>
+
+        <?php
+            echo "<h2>Result: $result</h2>";
+        ?>
     </main>
-</body>
 </html>
