f4a0cac92d8ec65d317eff7080c0d8072efad57c
Don Kieu <2177651@talnet.nl>
2024-12-19 14:19:28 +0100

    styling

--- Periode4/horeca/includes/nav.php
+++ Periode4/horeca/includes/nav.php
@@ -1,8 +1,11 @@
 <main>
-    <nav>
-        <a href="../website/index.php">Home</a>
-        <a href="../website/inlog.php">Log In</a>
-        <?php
-            //hier print andere links als er is ingelogd, overzicht om te wijzigen en uitloggen etc.    
-        ?>
-    </nav>
+    <div class="navigation">
+        <nav>
+            <h2>Jesse</h2>
+            <a href="../website/index.php">Home</a>
+            <a href="../website/inlog.php">Log In</a>
+            <?php
+                //hier print andere links als er is ingelogd, overzicht om te wijzigen en uitloggen etc.    
+            ?>
+        </nav>
+    </div>
---
+++ Periode4/horeca/website/index.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    include "../includes/db.php";
+    include "../includes/header.php";
+    include "../includes/nav.php";
+?>
+
+<div class="content">
+    <div class="container">
+        <h1>Jesse</h1>
+        <p>jesse</p>
+    </div>
+</div>
+
+<?php
+    include "../includes/footer.php";
+?>
