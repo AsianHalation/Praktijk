78bc8cb5b1341652f4d46f2b78f4faee4cf14a85
Don Kieu <2177651@talnet.nl>
2024-12-19 13:23:34 +0100

    horeca start laatste project p4

---
+++ Periode4/horeca/includes/nav.php
@@ -0,0 +1,8 @@
+<main>
+    <nav>
+        <a href="../website/index.php">Home</a>
+        <a href="../website/inlog.php">Log In</a>
+        <?php
+            //hier print andere links als er is ingelogd, overzicht om te wijzigen en uitloggen etc.    
+        ?>
+    </nav>
